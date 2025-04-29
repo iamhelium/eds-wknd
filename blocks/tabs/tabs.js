ee50f2bd0bc1e5b31cb8fbada1ff6d3f0f55dc8c
Vijay Rawat <vijay.rawat@iamhelium.com.au>
2025-04-29 23:30:48 +0530

    update tabs js

--- blocks/tabs/tabs.js
+++ blocks/tabs/tabs.js
@@ -1,3 +1,11 @@
-export default async function decorate(block) {
-  console.log(block);
+export default function decorate(block) {
+  const tabDiv = block.querySelector(':scope > div');
+
+  if (tabDiv) {
+    tabDiv.setAttribute('data-aue-type', 'container');
+    tabDiv.setAttribute('data-aue-behavior', 'component');
+    tabDiv.setAttribute('data-block-name', 'tab');
+    tabDiv.setAttribute('data-block-status', 'loaded');
+    tabDiv.className = 'tab block';
+  }
 }
