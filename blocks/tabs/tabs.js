72de6a5fffcce9a250fd9436da6e8957e24c7a30
Vijay Rawat <vijay.rawat@iamhelium.com.au>
2025-05-02 23:35:16 +0530

    update tab

--- blocks/tabs/tabs.js
+++ blocks/tabs/tabs.js
@@ -1,37 +1,37 @@
-// import {
-//   decorateBlock,
-//   decorateBlocks,
-//   loadBlock,
-// } from '../../scripts/aem.js';
+import {
+  decorateBlock,
+  decorateBlocks,
+  loadBlock,
+} from '../../scripts/aem.js';
 
 export default async function decorate(block) {
-  console.log('TABS BLOCK: ', block);
+  // console.log('TABS BLOCK: ', block);
 
-  // // Get all direct children that are tab components
-  // const tabBlocks = [...block.children].filter((child) => child.dataset?.aueModel === 'tab');
+  // Get all direct children that are tab components
+  const tabBlocks = [...block.children].filter((child) => child.dataset?.aueModel === 'tab');
 
-  // tabBlocks.forEach((tab) => {
-  //   // Ensure correct block-like behavior
-  //   tab.classList.add('tab', 'block');
-  //   tab.dataset.blockName = 'tab';
-  //   tab.dataset.blockStatus = 'initialized';
+  tabBlocks.forEach((tab) => {
+    // Ensure correct block-like behavior
+    tab.classList.add('tab', 'block');
+    tab.dataset.blockName = 'tab';
+    tab.dataset.blockStatus = 'initialized';
 
-  //   // Set AEM editing attributes
-  //   tab.dataset.aueType = 'container';
-  //   tab.dataset.aueBehavior = 'component';
+    // Set AEM editing attributes
+    tab.dataset.aueType = 'container';
+    tab.dataset.aueBehavior = 'component';
 
-  //   // Add expected wrapping if inner content isn't inside <p>
-  //   const firstChild = tab.querySelector('div');
-  //   if (firstChild && firstChild.childNodes.length && !firstChild.querySelector('p')) {
-  //     const wrapper = document.createElement('p');
-  //     wrapper.append(...firstChild.childNodes);
-  //     firstChild.appendChild(wrapper);
-  //   }
+    // Add expected wrapping if inner content isn't inside <p>
+    const firstChild = tab.querySelector('div');
+    if (firstChild && firstChild.childNodes.length && !firstChild.querySelector('p')) {
+      const wrapper = document.createElement('p');
+      wrapper.append(...firstChild.childNodes);
+      firstChild.appendChild(wrapper);
+    }
 
-  //   // Treat tab as a block and decorate its children blocks
-  //   decorateBlock(tab);
-  //   decorateBlocks(tab);
-  // });
+    // Treat tab as a block and decorate its children blocks
+    decorateBlock(tab);
+    decorateBlocks(tab);
+  });
 
-  // await Promise.all(tabBlocks.map(loadBlock));
+  await Promise.all(tabBlocks.map(loadBlock));
 }
