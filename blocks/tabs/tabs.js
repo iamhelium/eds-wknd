bc8298b86e88a71253bf9874b47c21e336b2654c
Vijay Rawat <vijay.rawat@iamhelium.com.au>
2025-05-04 21:39:38 +0530

    update tabs js

--- blocks/tabs/tabs.js
+++ blocks/tabs/tabs.js
@@ -8,31 +8,31 @@ export default async function decorate(block) {
   console.log('TABS BLOCK: ', block);
 
   // Get all direct children that are tab components
-  // const tabBlocks = [...block.children].filter((child) => child.dataset?.aueModel === 'tab');
+  const tabBlocks = [...block.children].filter((child) => child.dataset?.aueModel === 'tab');
 
   // console.log(tabBlocks);
-  // tabBlocks.forEach((tab) => {
-  //   // Ensure correct block-like behavior
-  //   tab.classList.add('tab', 'block');
-  //   tab.dataset.blockName = 'tab';
-  //   tab.dataset.blockStatus = 'initialized';
+  tabBlocks.forEach((tab) => {
+    // Ensure correct block-like behavior
+    tab.classList.add('tab', 'block');
+    tab.dataset.blockName = 'tab';
+    tab.dataset.blockStatus = 'initialized';
 
-  //   // Set AEM editing attributes
-  //   tab.dataset.aueType = 'container';
-  //   tab.dataset.aueBehavior = 'component';
+    // Set AEM editing attributes
+    tab.dataset.aueType = 'container';
+    tab.dataset.aueBehavior = 'component';
 
-  //   // Add expected wrapping if inner content isn't inside <p>
-  //   const firstChild = tab.querySelector('div');
-  //   if (firstChild && firstChild.childNodes.length && !firstChild.querySelector('p')) {
-  //     const wrapper = document.createElement('p');
-  //     wrapper.append(...firstChild.childNodes);
-  //     firstChild.appendChild(wrapper);
-  //   }
+    // Add expected wrapping if inner content isn't inside <p>
+    const firstChild = tab.querySelector('div');
+    if (firstChild && firstChild.childNodes.length && !firstChild.querySelector('p')) {
+      const wrapper = document.createElement('p');
+      wrapper.append(...firstChild.childNodes);
+      firstChild.appendChild(wrapper);
+    }
 
-  //   // Treat tab as a block and decorate its children blocks
-  //   // decorateBlock(tab);
-  //   // decorateBlocks(tab);
-  // });
+    // Treat tab as a block and decorate its children blocks
+    // decorateBlock(tab);
+    // decorateBlocks(tab);
+  });
 
   // await Promise.all(tabBlocks.map(loadBlock));
 }
