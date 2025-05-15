0dd36d720212b1ec3d7ac0f74d10fff70a55add1
Surabhi-helium <surabhi@iamhelium.com.au>
2025-05-15 10:30:44 +0530

    added

--- blocks/subnav/subnav.js
+++ blocks/subnav/subnav.js
@@ -9,6 +9,54 @@ export default function decorate(block) {
   languageText.className = 'language-text';
   languageText.textContent = 'EN-US';
 
+  const languageDropdown = document.createElement('div');
+  languageDropdown.className = 'language-dropdown';
+  languageDropdown.innerHTML = `
+<div class="language-option">
+<div><img src="us-flag.png" alt="US Flag" class="flag-icon"> UNITED STATES</div>
+<div class="language-codes">EN-US | ES-US</div>
+</div>
+<div class="language-option">
+<div><img src="canada-flag.png" alt="Canada Flag" class="flag-icon"> CANADA</div>
+<div class="language-codes">EN-CA | FR-CA</div>
+</div>
+<div class="language-option">
+<div><img src="switzerland-flag.png" alt="Swiss Flag" class="flag-icon"> SWITZERLAND</div>
+<div class="language-codes">DE-CH | FR-CH | IT-CH</div>
+</div>
+<div class="language-option">
+<div><img src="germany-flag.png" alt="German Flag" class="flag-icon"> GERMANY</div>
+<div class="language-codes">DE-DE</div>
+</div>
+<div class="language-option">
+<div><img src="france-flag.png" alt="France Flag" class="flag-icon"> FRANCE</div>
+<div class="language-codes">FR-FR</div>
+</div>
+<div class="language-option">
+<div><img src="spain-flag.png" alt="Spain Flag" class="flag-icon"> SPAIN</div>
+<div class="language-codes">ES-ES</div>
+</div>
+<div class="language-option">
+<div><img src="italy-flag.png" alt="Italy Flag" class="flag-icon"> ITALY</div>
+<div class="language-codes">IT-IT</div>
+</div>
+`;
+
+  languageDropdown.style.display = 'none';
+  // eslint-disable-next-line no-use-before-define
+  navWrapper.appendChild(languageDropdown);
+  // Toggle dropdown
+  languageText.addEventListener('click', () => {
+    const isVisible = languageDropdown.style.display === 'block';
+    languageDropdown.style.display = isVisible ? 'none' : 'block';
+  });
+  // Hide dropdown when clicking outside
+  document.addEventListener('click', (e) => {
+    if (!languageText.contains(e.target) && !languageDropdown.contains(e.target)) {
+      languageDropdown.style.display = 'none';
+    }
+  });
+
   const navWrapper = document.createElement('div');
   navWrapper.className = 'subnav-wrapper';
 
