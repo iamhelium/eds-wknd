85b0fa11e50a23f0fdf94272bf370a06367a4544
Vijay Rawat <vijay.rawat@iamhelium.com.au>
2025-05-16 10:54:23 +0530

    update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,7 @@ module.exports = {
     'xwalk/max-cells': ['error', {
       '*': 5,
       teaser: 10, // custom rule for specific model
-      articles: 10,
+      articles: 15,
       form: 15,
       wizard: 12,
       'form-button': 7,
